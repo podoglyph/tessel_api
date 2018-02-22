0c790b0a5653d479fc4f3b29c7a663dec8b869d5
Padraic O'Neill <pconeill@gmail.com [core]>
2018-02-21 21:56:36 -0700

    testing https

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 8000;
 const db = require('./config/db');
 const LedsController = require('./lib/controllers/ledsController');
-const TesselBoard = 'http://stark-tesselator.local:8888';
+if (process.env.NODE_ENV === "production") {
+  const TesselBoard = process.env.IP;
+} else {
+  const TesselBoard = 'http://stark-tesselator.local:8888';
+}
 
-app.use(express.static('client'));
+// app.use('/static', express.static(path.join(__dirname, 'client/build')));
 
 app.use('/leds', LedsController);
 
 app.use('/camera', function(req, res, err) {
-  res.redirect(`${TesselBoard}/camera`);
+  res.redirect(TesselBoard + '/camera');
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, function() {
   console.log('Express server listening on port ' + port);
