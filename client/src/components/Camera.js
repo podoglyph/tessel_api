28d3c6827f1afb7b42fba05bc97363ab96d53359
Padraic O'Neill <pconeill@gmail.com [core]>
2018-02-22 01:37:45 -0700

    debug camera

--- client/src/components/Camera.js
+++ client/src/components/Camera.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Time from './Time.js';
 
-const stream = 'https://65.128.75.135:43333/camera';
+
 
 function Video() {
 
@@ -11,7 +11,7 @@ function Video() {
       <div className="image-overlay">
         <div className="image-overlay-data">
           <Time />
-          <img alt="Camera feed" src={stream} />
+          <img alt="Camera feed" src="https://65.128.75.135:43333/camera"/>
         </div>
       </div>
     </div>
