31fbd4580c885cf88dca77d91507aab3d5bb3be7
Padraic O'Neill <pconeill@gmail.com [core]>
2018-02-25 16:54:36 -0700

    add API GET test for leds using chai and chai-http

---
+++ client/test/led_api_test.js
@@ -0,0 +1,18 @@
+const chai = require('chai');
+const assert = chai.assert;
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+const API = 'http://localhost:8000';
+
+describe('GET /leds', () => {
+    it('should return array of 4 elements', done => {
+      chai.request(API)
+        .get('/leds')
+        .end( (err, res) => {
+          assert.equal(res.status, 200);
+          assert.isArray(res.body);
+          assert.equal(res.body.length, 4);
+          done();
+        });
+    });
+  });
