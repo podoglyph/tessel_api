3b4518671d08c62b522c237deb3748f582725dac
Padraic O'Neill <pconeill@gmail.com [core]>
2018-02-25 17:04:45 -0700

    add assertions for id, color, index, and isOn in Led GET request rest

--- client/test/led_api_test.js
+++ client/test/led_api_test.js
@@ -5,13 +5,17 @@ chai.use(chaiHttp);
 const API = 'http://localhost:8000';
 
 describe('GET /leds', () => {
-  it('should return array of 4 elements', done => {
+  it('should return array of 4 elements representing onboard leds', done => {
     chai.request(API)
       .get('/leds')
       .end( (err, res) => {
         assert.equal(res.status, 200);
         assert.isArray(res.body);
         assert.equal(res.body.length, 4);
+        assert.typeOf(res.body[0]._id, 'string');
+        assert.typeOf(res.body[0].color, 'string');
+        assert.typeOf(res.body[0].index, 'string');
+        assert.typeOf(res.body[0].isOn, 'boolean');
         done();
       });
   });
